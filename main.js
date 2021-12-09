3b51903d42895651399b544b80ff57bacbec5652
Fatemeh <fatemeh211100@gmail.com>
2021-12-09 15:23:27 +0330

    tamrin

---
+++ main.js
@@ -0,0 +1,24 @@
+const centerPoint = {
+    x: 400,
+    y: 200,
+};
+const eyeOne ={
+    x:350,
+    y:150,
+};
+const eyeTwo ={
+    x:450,
+    y:150,
+};
+const startPoint = {
+    x:450,
+    y:200,
+}
+const endPoint = {
+    x:350,
+    y:200,
+}
+drawCircle(centerPoint,100)
+drawCircle(eyeOne,4)
+drawCircle(eyeTwo,4)
+drawLine(startPoint,endPoint)
